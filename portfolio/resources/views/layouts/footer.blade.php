28d9ad1ead4e8b66427c75c336192d26730b1236
notarock <roch.damour@gmail.com>
2017-05-31 17:14:44 -0400

    apparence

---
+++ portfolio/resources/views/layouts/footer.blade.php
@@ -0,0 +1,30 @@
+<!-- +++++ Footer Section +++++ -->
+
+<div id="footer">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-4">
+				<h4>Ou me retrouver</h4>
+				<p>
+					
+				</p>
+			</div><!-- /col-lg-4 -->
+
+			<div class="col-lg-4">
+				<h4>My Links</h4>
+				<p>
+					<a href="#">Dribbble</a><br/>
+					<a href="#">Twitter</a><br/>
+					<a href="#">Facebook</a>
+				</p>
+			</div><!-- /col-lg-4 -->
+
+			<div class="col-lg-4">
+				<h4></h4>
+				<p>This cute theme was created to showcase your work in a simple way. Use it wisely.</p>
+			</div><!-- /col-lg-4 -->
+			
+		</div>
+		
+	</div>
+</div>
---
+++ portfolio/resources/views/layouts/master.blade.php
@@ -0,0 +1,38 @@
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="../../docs-assets/ico/favicon.png">
+
+    <title>Portfolio de Roch D'Amour</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="/css/bootstrap.css" rel="stylesheet">
+
+
+    <!-- Custom styles -->
+    <link href="/css/main.css" rel="stylesheet">
+
+    <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+    <script src="/js/hover.zoom.js"></script>
+    <script src="/js/hover.zoom.conf.js"></script>
+
+</head>
+
+<body>
+
+    @include('layouts.nav')
+
+    <div class="container">
+        @yield('content')
+    </div>
+
+    @include('layouts.footer')
+
+</body>
+<!-- Bootstrap core JavaScript -->
+<script src="/js/bootstrap.min.js"></script>
+</html>
