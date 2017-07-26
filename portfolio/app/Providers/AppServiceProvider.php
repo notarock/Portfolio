bb31bffb42c6ed2edf13ccbe31cd431a41e3353e
notarock <roch.damour@gmail.com>
2017-07-26 13:09:58 -0400

    combobox appserviceproviders

--- portfolio/app/Providers/AppServiceProvider.php
+++ portfolio/app/Providers/AppServiceProvider.php
@@ -4,34 +4,42 @@ namespace App\Providers;
 use Carbon\Carbon;
 use App\Categorie;
 use App\User;
+use App\Cadre;
+use App\Etat;
 
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-	    Carbon::setLocale('fr');
-	    view()->composer('layouts.nav', function ($view){
-		    $view->with('DDcategories',Categorie::DDcategories());
-	    });
-	    view()->composer('layouts.footer', function($view){
-		    $view->with('liens', User::liens());
-	    });
-} 
-		
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-       
-    }
+	/**
+	 * Bootstrap any application services.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		Carbon::setLocale('fr');
+		view()->composer('layouts.nav', function ($view){
+			$view->with('DDcategories',Categorie::DDcategories());
+		});
+		view()->composer('layouts.footer', function($view){
+			$view->with('liens', User::liens());
+		});
+		view()->composer('projets.editable', function($view){
+			$view->with('CBcategories', Categorie::CBcategories ());
+			
+			$view->with('CBcadres', Cadre::BDcadres());
+			$view->with('CBetats', Etat::BDetats());
+		});
+	} 
+
+	/**
+	 * Register any application services.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+
+	}
 }
