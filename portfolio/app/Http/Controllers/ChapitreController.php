844511902820352769919f7f63b6db13423d1707
Roch D'Amour <roch.damour@gmail.com>
2017-10-26 00:45:06 -0400

    edit works

--- portfolio/app/Http/Controllers/ChapitreController.php
+++ portfolio/app/Http/Controllers/ChapitreController.php
@@ -3,21 +3,144 @@
 namespace App\Http\Controllers;
 use App\Chapitre;
 use App\Projet;
-
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Illuminate\Support\Facades\Auth;
+use Session;
 use Illuminate\Http\Request;
+
 
 class ChapitreController extends Controller
 {
 
-	public function show (Projet $projet, int $id){
+	public function __construct()
+	{
+		$this->middleware('isAdmin')->except( 'show');
+	}
+
+	public function show (Projet $projet, Chapitre $chapitre){
+
+		return view('chapitres.show', compact ('chapitre', 'projet'));
+
+	}
+
+	public function update(Projet $projet,Chapitre $chapitre, Request $request)
+	{
+
+		$this->validate($request, [
+			'name' => 'required',
+			'textUp' => 'required',
+			'textDown' => 'required',
+		]);	
+
+		if ($request->hasFile('picture')) {
+			$image      = $request->file('picture');
+			$fileName   = $request->file('picture')->getClientOriginalName();		
+
+			$image->move("img/chapitres/" . $chapitre->projet_id . "/",$fileName);	
+
+			$chapitre->picture = $fileName;
+		} 
+
+		$chapitre->name = $request->name;
+		$chapitre->textUp = $request->textUp;
+		$chapitre->textDown = $request->textDown;
+
+		$chapitre->update();
+
+		Session::flash('status', 'Chapitre ' . $chapitre->name . ' a été modifié!');
+		return redirect('/projets/' . $projet->id . "/chapitres/" . $chapitre->id);
+	}	
+
+	/**
+	 * Affiche la liste des catégories.
+	 *
+	 * @return View qui affiche 12 catégories par pages.
+	 */
+	public function index()
+	{	
+		$lstChapitres = Chapitre::orderBy('updated_at','DESC')->paginate(12);
+
+		return view('chapitres.index', compact('lstChapitres'));
+	}
+
+	/**
+	 * Affiche la page de création d'un nouveau projet
+	 * 
+	 * @return Formulaire de création d'une catégorie
+	 */
+	public function create()
+	{
+
+		$chapitre = new Chapitre();
+
+		return view('chapitres.create', compact('chapitre'));
+	}
 
 
+	/**
+	 * Enregistre la nouvelle chapitre dans la base de données
+	 *
+	 * @return Page show de la nouvelle chapitre et status qui prouve que la catégorie
+	 *  a bien été créée
+	 */
+	public function store(Request $request)
+	{	
 
-		$chapitre = Chapitre::find($id);
+		$this->validate($request, [
+			'name' => 'required|max:100',
+			'description' => 'required',
+			'picture' => 'required',	
+		]);
+
+		$chapitre = new Chapitre;
+
+		if ($request->hasFile('picture')) {
+			$image      = $request->file('picture');
+			$fileName   = $request->file('picture')->getClientOriginalName();	
+
+			$image->move("img/chapitres/",$fileName);	
+
+			$chapitre->picture = $fileName;
+		} 
+
+		$chapitre->name = $request->name;
+		$chapitre->description = $request->description;
+
+		$chapitre->save();
+
+		Session::flash('status', 'Chapitre ' . $chapitre->name . ' a été créer!');
+		return redirect('/chapitres/' . $chapitre->id);
+
+	}
+
+	/**
+	 *
+	 */	
+	public function edit(Projet $projet, Chapitre $chapitre)
+	{
+
+		return view('chapitres.edit', compact('chapitre'));
+
+	}
 
 
+	public function delete(Chapitre $chapitre)
+	{
+		return view('chapitres.delete', compact('chapitre'));
+	}
 
-		return view('chapitres.show', compact ('chapitre'));
-		
+
+	public function destroy(Chapitre $chapitre)
+	{
+		$nomChapitre = $chapitre->name;
+
+		$chapitre->delete();
+
+		Session::flash('status', 'La catégorie ' 
+			. $nomChapitre .
+			' a été supprimée définitivement!');
+
+		return Redirect('/chapitres' );
 	}
 }
+
--- portfolio/resources/views/chapitres/show.blade.php
+++ portfolio/resources/views/chapitres/show.blade.php
@@ -10,7 +10,7 @@
 
 		<div class="row mt">
 			<div class="col-md-8 col-md-offset-2 centered">
-				<h3>{{$chapitre->projet->name}}</h3>
+				<h3>{{$projet->name}}</h3>
 				<hr />
 			</div>
 		</div>
@@ -19,11 +19,11 @@
 		@if(Auth::user()->admin)
 			<div class="col-md-8 col-md-offset-2 centered">
 				<div class="row">
-					<a href='/projets/{{$projet->id}}/edit'
+					<a href='/projets/{{$projet->id}}/chapitres/{{$chapitre->id}}/edit'
 					class="col-md-4 col-md-offset-2 btn btn-warning">
 						<span style="color:black;">Éditer</span>
 					</a>
-					<a href='/projets/{{$projet->id}}/delete' class="col-md-4 btn btn-danger">
+					<a href='/projets/{{$projet->id}}/chapitre/{{$chapitre->id}}/delete' class="col-md-4 btn btn-danger">
 						<span style="color:black;">Supprimer</span>
 					</a>
 				</div>
@@ -46,7 +46,7 @@
 		<div class="row mt">
 			<div class="col-md-10 col-md-offset-1 centered">
 
-				<p><img class="img-responsive" src="/img/chapitres/{{$chapitre->picture}}" alt="" /></p>
+				<p><img class="img-responsive" src="/img/chapitres/{{$chapitre->projet_id}}/{{$chapitre->picture}}" alt="" /></p>
 
 			</div>
 		</div>
